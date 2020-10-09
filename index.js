01920e1f9a6dd37a14467a8fddcbce52536852b5
Helio Frota <00hf11@gmail.com>
2020-10-09 11:15:34 -0300

    using same style as pkgjs projects

--- index.js
+++ index.js
@@ -1,85 +1,85 @@
-'use strict';
+'use strict'
 
-const { join } = require('path');
-const fs = require('fs');
+const { join } = require('path')
+const fs = require('fs')
 
 const hasPackageJson = (cwd, handleIt) => {
   if (!fs.existsSync(join(cwd, 'package.json'))) {
     if (handleIt) {
-      console.error('This is not a Node.js project (no package.json found).');
-      process.exit(1);
+      console.error('This is not a Node.js project (no package.json found).')
+      process.exit(1)
     } else {
-      return false;
+      return false
     }
   }
-  return true;
-};
+  return true
+}
 
 const hasAnyDependencies = (cwd, handleIt) => {
-  let errors = 0;
+  let errors = 0
   if (!require(`${cwd}/package.json`).dependencies) {
-    errors++;
+    errors++
   }
   if (!require(`${cwd}/package.json`).devDependencies) {
-    errors++;
+    errors++
   }
   if (errors === 2) {
     if (handleIt) {
-      console.error('The project has no dependencies declared.');
-      process.exit(1);
+      console.error('The project has no dependencies declared.')
+      process.exit(1)
     } else {
-      return false;
+      return false
     }
   }
-  return true;
-};
+  return true
+}
 
 const hasDevDependencies = (cwd, handleIt) => {
   if (!require(`${cwd}/package.json`).devDependencies) {
     if (handleIt) {
-      console.error('The project has no dev dependencies declared.');
-      process.exit(1);
+      console.error('The project has no dev dependencies declared.')
+      process.exit(1)
     } else {
-      return false;
+      return false
     }
   }
-  return true;
-};
+  return true
+}
 
 const hasDependencies = (cwd, handleIt) => {
   if (!require(`${cwd}/package.json`).dependencies) {
     if (handleIt) {
-      console.error('The project has no dependencies declared.');
-      process.exit(1);
+      console.error('The project has no dependencies declared.')
+      process.exit(1)
     } else {
-      return false;
+      return false
     }
   }
-  return true;
-};
+  return true
+}
 
 const hasNodeModules = (cwd, handleIt) => {
-  const modulesDir = join(cwd, 'node_modules');
+  const modulesDir = join(cwd, 'node_modules')
   if (fs.existsSync(modulesDir)) {
-    const content = fs.readdirSync(modulesDir).filter(e => e !== '.bin');
+    const content = fs.readdirSync(modulesDir).filter(e => e !== '.bin')
     if (content.length === 0) {
       if (handleIt) {
-        console.error('No module installed. Please run npm install.');
-        process.exit(1);
+        console.error('No module installed. Please run npm install.')
+        process.exit(1)
       } else {
-        return false;
+        return false
       }
     }
   } else {
     if (handleIt) {
-      console.error('Directory node_modules not found. Please run npm install.');
-      process.exit(1);
+      console.error('Directory node_modules not found. Please run npm install.')
+      process.exit(1)
     } else {
-      return false;
+      return false
     }
   }
-  return true;
-};
+  return true
+}
 
 module.exports = {
   hasPackageJson,
@@ -87,4 +87,4 @@ module.exports = {
   hasDependencies,
   hasDevDependencies,
   hasAnyDependencies
-};
+}
